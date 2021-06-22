6840afa046aa9795bfae321b7e887a4749498cb3
Roberto <betobr12@users.noreply.github.com>
2021-06-22 12:25:20 -0300

    Update post.service.ts

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -3,14 +3,10 @@ import { Injectable } from '@angular/core';
 import { Post } from './post';
 
 @Injectable({
-  providedIn: 'root'// esta injetado direto no servise do appmodule
+  providedIn: 'root'
 })
 export class PostService {
-
   public posts: Post[] = [ ];
-
-  //parte de serviços para acesso a api laravel, importante criar um serviço para cada modulo
-
   constructor(private http: HttpClient) { 
     this.http.get("/api").subscribe(
       (posts: any[])=>{
@@ -26,17 +22,15 @@ export class PostService {
   }
   salvar(post: Post, file: File){
     const uploadData = new FormData();
-    uploadData.append('nome',post.nome); //local da api, local do front
-    uploadData.append('titulo',post.titulo); //local da api, local do front
-    uploadData.append('subtitulo',post.subtitulo); //local da api, local do front
-    uploadData.append('email',post.email); //local da api, local do front
-    uploadData.append('mensagem',post.mensagem); //local da api, local do front
-    uploadData.append('arquivo',file, file.name); //local da api, local do front
-
+    uploadData.append('nome',post.nome); 
+    uploadData.append('titulo',post.titulo); 
+    uploadData.append('subtitulo',post.subtitulo);
+    uploadData.append('email',post.email); 
+    uploadData.append('mensagem',post.mensagem);
+    uploadData.append('arquivo',file, file.name); 
     this.http.post("/api", uploadData, { reportProgress:true, observe: 'events' })
     .subscribe((event: any)=>{
       if(event.type == HttpEventType.Response){
-       // console.log(event);
        let p: any = event.body;
        this.posts.push(
         new Post(
@@ -53,22 +47,17 @@ export class PostService {
 
   like(id: number){
     this.http.get('/api/like/'+id).subscribe(
-      (event: any)=>{
-        //console.log(event);
+      (event: any)=>{     
         let p =  this.posts.find((p)=> p.id == id );
         p.likes = event.likes;
-
       }
     )
   }
-
   apagar(id: number){
     this.http.delete("/api/"+id).subscribe(event =>{
-     // console.log(event);
      let i =  this.posts.findIndex((p)=> p.id == id );
      if(i >=0 )
      this.posts.splice(i,1);
-
     })
   }
 }
